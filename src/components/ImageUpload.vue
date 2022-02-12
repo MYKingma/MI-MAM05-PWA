781c275df464822fe4b2583c8893875fe1d600dc
MYKingma <55245466+MYKingma@users.noreply.github.com>
2022-02-12 15:15:35 +0100

    chore: implement image upload component, testing

---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-main>
+    <el-upload
+      ref="upload"
+      class="upload-demo"
+      :limit="1"
+      :on-exceed="handleExceed"
+      :auto-upload="false"
+      accept="image/*"
+      capture="camera"
+    >
+      <template #trigger>
+        <el-button type="primary">select file</el-button>
+      </template>
+      <el-button class="ml-3" type="success" @click="submitUpload"
+        >upload to server</el-button
+      >
+      <template #tip>
+        <div class="el-upload__tip" style="color: red">
+          limit 1 file, new file will cover the old file
+        </div>
+      </template>
+    </el-upload>
+  </el-main>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      image: null,
+    };
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    handleExceed(files) {
+      this.image.value.clearFiles();
+      this.image.value.handleStart(files[0]);
+    },
+    submitUpload() {
+      this.image.value.submit();
+    },
+  },
+  watch: {},
+};
+</script>
+
+<style lang="scss"></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,13 +6,59 @@
       titel veranderd). <br />
       Zo niet, dan kom je op de homepagina uit.
     </p>
+    <br />
+    <el-collapse v-model="activeSection">
+      <el-collapse-item title="Photo upload and preview" name="1">
+        <ImageUpload />
+      </el-collapse-item>
+      <!-- <el-collapse-item title="Feedback" name="2">
+        <div>
+          Operation feedback: enable the users to clearly perceive their
+          operations by style updates and interactive effects;
+        </div>
+        <div>
+          Visual feedback: reflect current state by updating or rearranging
+          elements of the page.
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Efficiency" name="3">
+        <div>
+          Simplify the process: keep operating process simple and intuitive;
+        </div>
+        <div>
+          Definite and clear: enunciate your intentions clearly so that the
+          users can quickly understand and make decisions;
+        </div>
+        <div>
+          Easy to identify: the interface should be straightforward, which helps
+          the users to identify and frees them from memorizing and recalling.
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Controllability" name="4">
+        <div>
+          Decision making: giving advices about operations is acceptable, but do
+          not make decisions for the users;
+        </div>
+        <div>
+          Controlled consequences: users should be granted the freedom to
+          operate, including canceling, aborting or terminating current
+          operation.
+        </div>
+      </el-collapse-item> -->
+    </el-collapse>
   </el-main>
 </template>
 
 <script>
+import ImageUpload from "../components/ImageUpload.vue";
 export default {
   data() {
-    return {};
+    return {
+      activeSection: 1,
+    };
+  },
+  components: {
+    ImageUpload,
   },
   computed: {},
   async mounted() {},
